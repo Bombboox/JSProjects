9b17faa09fd454467b45e11695bc9d4f44d5b72d
Arman Petrosyan <79475041+Bombboox@users.noreply.github.com>
2023-12-01 21:14:30 -0800

    weight mutations added

--- JavaScript Neat Algorithm/connection.js
+++ JavaScript Neat Algorithm/connection.js
@@ -6,4 +6,29 @@ class Connection {
         this.enabled = enabled; // Boolean indicating whether the connection is enabled
         this.innovation = innovation; // Unique identifier for the connection
     }
+
+    mutateWeight(mutationRate) {
+        let rand = random(0, 1);
+        let a = 0.02; //Probability of case 1 
+        let b = 0.02; //Probability of case 2
+        let c = 0.02; //Probability of case 3
+
+        if(rand < a) { //Replace with new value
+            this.weight = random(-1, 1);
+            return 0;
+        }
+
+        if(rand < a + b) { //Multiply by random percentage
+            this.weight *= random(0.5, 1.5);
+            return 0;
+        }
+
+        if(rand < a + b + c) { //Add random number
+            this.weight += random(-1, 1);
+            return 0;
+        }
+
+        //Add random gaussian number gigachad
+        this.weight += gaussianRandom() * mutationRate;
+    }
 }
--- JavaScript Neat Algorithm/math.js
+++ JavaScript Neat Algorithm/math.js
@@ -7,3 +7,10 @@ const randint = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 const max = (a, b) => a > b ? a : b;
 const min = (a, b) => a < b ? a : b;
 const clamp = (value, min, max) => (value < min ? min : value > max ? max : value);
+const gaussianRandom = (mean=0, stdev=1) => {
+    const u = 1 - Math.random();
+    const v = Math.random();
+    const z = Math.sqrt( -2.0 * Math.log( u ) ) * Math.cos( 2.0 * Math.PI * v );
+
+    return z * stdev + mean;
+}
